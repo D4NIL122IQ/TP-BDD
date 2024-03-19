c70b2f13d27a9497bd7afa60b941e435af023ae2
Danilgdj <danilguidjou7@gmail.com>
2024-03-19 03:11:51 +0100

    finieLaSerieTP

--- tp1.sql
+++ tp1.sql
@@ -19,7 +19,7 @@ CREATE TABLE IF NOT EXISTS module (
     PRIMARY KEY (codeModule)
 )
 
-CREATE TABLE if7 NOT EXISTS suivre (
+CREATE TABLE if NOT EXISTS suivre (
     idPer VARCHAR(5),
     idMod VARCHAR(5),
     note int check (note<=20 && note>=0)
@@ -43,4 +43,48 @@ ALTER TABLE personne MODIFY adresseP VARCHAR(50) DEFAULT 'algerie'
 -----------------------------------------------------7
 ALTER TABLE module ADD CONSTRAINT ck_pc CHECK (coefM >=0)
 
+-----------------------------------------------------8
+INSERT INTO personnes 
+VALUES ("1MI",11992,"KABLI","Cilia","1992/01/20","Bejaia"),
+       ("2MI",21992,"ADI","Mounir","1991/03/25","Bouira"),
+       ("3MI",31993,"HAMOUDI",NULL,"1993/08/14","Bejaia"),--! idPersonne '2MI' est deja existant donc faut le modifier car la clé primaire est unique
+       (NULL,51989,"KASRI","Amine","1989/05/01","Bejaia"),
+       (10,31980,"KARA","Lyes","1980/05/18","Alger"),
+       (11,41981,"SALMI","Riad","1981/01/09"," "),
+       (13,41985,NULL,"Ryma","1985/02/26","Bejaia");
 
+--------------------------------------------------------9
+INSERT INTO modules 
+VALUES  ("BDL2","Bases de donnees",3,"10"),
+        ("SEL3","Systeme d`exploitation",2,"11"),
+        ("SEL2","Systeme d`information",4,"11"),
+        ("1MI","BDL2",NULL,"15"),
+        (NULL,"SEL3",NULL,"11");
+
+-----------------------------------------------------10
+INSERT INTO suivre 
+VALUES  ("1MI","SEL3",17),
+        ("2MI","SEL2",12),
+        ("2MI","LML2",13),
+        ("3MI","SEL3",11);
+
+---------------------------------------------------------11
+UPDATE personne set NumSec = 31980 WHERE idPersonne = '11' ---confili NumSec est unique 
+
+------------------------------------------------------------12
+UPDATE module set codeModule ='BDDL2' WHERE codeModule = 'BDL2'
+
+------------------------------------------------------------13
+DELETE FROM module WHERE codeModule ='SEL3'
+
+------------------------------------------------------------14
+UPDATE personne set idPersonne = '11MI' WHERE idPersonne = '1MI'
+
+------------------------------------------------------------15
+DELETE FROM personne WHERE idPersonne = '2MI'
+
+------------------------------------------------------------16
+UPDATE personne set idPersonne ='21' WHERE idPersonne ='11'
+
+--------------------------------------------------------------17
+DELETE FROM personne WHERE idPersonne ='10'
